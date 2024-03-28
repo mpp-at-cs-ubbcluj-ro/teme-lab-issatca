399c3232ea0b50dc5bc64e0eaab584b35f4526cb
Clarissa <clarissatelescu22@gmail.com>
2024-03-28 14:13:08 +0200

    Lab4

---
+++ TemaLab4/src/main/java/ro/mpp2024/RepairShopConfig.java
@@ -0,0 +1,52 @@
+package ro.mpp2024;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ro.mpp2024.Repository.ComputerRepairRequestRepository;
+import ro.mpp2024.Repository.ComputerRepairedFormRepository;
+import ro.mpp2024.Repository.jdbc.ComputerRepairRequestJdbcRepository;
+import ro.mpp2024.Repository.jdbc.ComputerRepairedFormJdbcRepository;
+import ro.mpp2024.services.ComputerRepairServices;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+public class RepairShopConfig {
+    @Bean
+    Properties getProps()
+    {
+        Properties props = new Properties();
+        try
+        {
+            System.out.println("Searching bd.config in directory " + ((new File(".")).getAbsolutePath()));
+            props.load(new FileReader("bd.config"));
+        }
+        catch (IOException e)
+        {
+            System.err.println("Configuration file bd.config not found " + e);
+        }
+        return props;
+    }
+
+    @Bean
+    ComputerRepairRequestRepository requestsRepo()
+    {
+        return new ComputerRepairRequestJdbcRepository(getProps());
+    }
+
+    @Bean
+    ComputerRepairedFormRepository formsRepo()
+    {
+        return new ComputerRepairedFormJdbcRepository(getProps());
+    }
+
+    @Bean
+    ComputerRepairServices services()
+    {
+        return new ComputerRepairServices(requestsRepo(), formsRepo());
+    }
+
+}
